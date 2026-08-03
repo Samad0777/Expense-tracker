3ed1476f02fa2ba1ed049265d21e40ed4ac5316e
Fardeen <sheikfardeen6@gmail.com>
2026-08-04 00:08:25 +0530

    feat : setup auth routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Register from './pages/Register'
-
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/AppRoutes";
 
 const App = () => {
   return (
     <>
-    <Register/>
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
