55601c9a2dc18e5b3c4206dde4117e7f9fd7e071
Fardeen <sheikfardeen6@gmail.com>
2026-08-10 19:40:46 +0530

    feat: complete analytics ui

--- frontend/src/components/Ui/Card.jsx
+++ frontend/src/components/Ui/Card.jsx
@@ -1,14 +1,14 @@
-const Card = ({ title, icon: Icon, amount, iconBg, iconColor }) => {
+const Card = ({ title, icon: Icon, amount, iconBg, iconColor, amountColor }) => {
   return (
     <div className="flex flex-col gap-4 bg-surface shadow-2xl p-4 rounded-2xl">
       <div className="flex items-center justify-between ">
         <h2 className="text-text-secondary">{title}</h2>
-        <div className={`${iconBg} px-2 py-2 rounded-xl`}>
+       {Icon && (<div className={`${iconBg} px-2 py-2 rounded-xl`}>
           <Icon className={iconColor} size={20} />
-        </div>
+        </div>)}
       </div>
       <div>
-        <p className="text-text-primary text-3xl font-bold">
+        <p className={`${amountColor} text-3xl font-bold`}>
           {"₹"}
           {amount}
         </p>
